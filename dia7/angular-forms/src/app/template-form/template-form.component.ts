a8e728314cb02aa386bd0f41c501578515c2fcc0
Andrés Arias <arias9306@gmail.com>
2019-12-12 15:09:31 -0500

    validaciones básicas TD

--- dia7/angular-forms/src/app/template-form/template-form.component.ts
+++ dia7/angular-forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { GenderService } from "../services/gender.service";
 
@@ -9,13 +9,25 @@ import { GenderService } from "../services/gender.service";
 })
 export class TemplateFormComponent implements OnInit {
   genders: string[] = [];
+
+  @ViewChild("authorForm", { static: false }) authorForm: NgForm;
+
   constructor(private genderService: GenderService) {}
 
   ngOnInit() {
     this.genders = this.genderService.getGenders();
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.authorForm);
+  }
+
+  onReset() {
+    this.authorForm.reset();
+    this.authorForm.form.patchValue({});
   }
 }
