032c29910ac2ed7dbced018edab6b40415bcd13e
Andrés Arias <arias9306@gmail.com>
2019-12-12 10:14:46 -0500

    Template Forms

--- dia7/angular-forms/src/app/app.module.ts
+++ dia7/angular-forms/src/app/app.module.ts
@@ -1,47 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { TemplateFormComponent } from './template-form/template-form.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { MatSelectModule } from '@angular/material/select';
-
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ReactiveFormComponent } from "./reactive-form/reactive-form.component";
+import { TemplateFormComponent } from "./template-form/template-form.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TemplateFormComponent,
-    ReactiveFormComponent
-  ],
+  declarations: [AppComponent, TemplateFormComponent, ReactiveFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatCardModule,
     MatToolbarModule,
-    FlexLayoutModule,
     MatInputModule,
     MatFormFieldModule,
-    FormsModule,
     MatButtonModule,
     MatIconModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatSelectModule
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- dia7/angular-forms/src/app/template-form/template-form.component.ts
+++ dia7/angular-forms/src/app/template-form/template-form.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { GenderService } from '../services/gender.service';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { GenderService } from "../services/gender.service";
 
 @Component({
-  selector: 'app-template-form',
-  templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.css']
+  selector: "app-template-form",
+  templateUrl: "./template-form.component.html",
+  styleUrls: ["./template-form.component.css"]
 })
 export class TemplateFormComponent implements OnInit {
-
   genders: string[] = [];
-  constructor(private genderService: GenderService) { }
+  constructor(private genderService: GenderService) {}
 
   ngOnInit() {
     this.genders = this.genderService.getGenders();
   }
 
-
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
